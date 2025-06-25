0cd956c0fbded952b59baf47608d91d2a928583a
Thanh The <pttheit@gmail.com>
2025-06-25 17:31:00 +0700

    update

--- src/main/java/com/theer/controller/UserController.java
+++ src/main/java/com/theer/controller/UserController.java
@@ -2,10 +2,10 @@ package com.theer.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.RequestMethod;
+import com.theer.domain.User;
 import com.theer.service.UserServive;
 
 @Controller
@@ -24,19 +24,17 @@ public class UserController {
         model.addAttribute("theer", "from controller with model");
         return "hello";
     }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        String test = this.userServive.handleHello();
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User theer) {
+        System.out.println("run here " + theer);
+        return "hello";
+    }
 }
-
-// @RestController
-// public class UserController {
-
-// private UserServive userServive;
-
-// public UserController(UserServive userServive) {
-// this.userServive = userServive;
-// }
-
-// @GetMapping("/")
-// public String getHomePage() {
-// return this.userServive.handleHello();
-// }
-// }
