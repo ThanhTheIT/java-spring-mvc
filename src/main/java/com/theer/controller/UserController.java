f830af4a26b39458fc3c14ba6c708ee77bc07d37
Thanh The <pttheit@gmail.com>
2025-06-26 01:32:01 +0700

    update

--- src/main/java/com/theer/controller/UserController.java
+++ src/main/java/com/theer/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import com.theer.domain.User;
@@ -34,6 +35,12 @@ public class UserController {
         List<User> users = this.userServive.getAllUsers();
         model.addAttribute("users", users);
         return "admin/user/table_user";
+    }
+
+    @RequestMapping("/admin/user/{id}")
+    public String getUserDetailPage(Model model, @PathVariable long id) {
+        model.addAttribute("id", id);
+        return "admin/user/user_detail";
     }
 
     @RequestMapping("/admin/user/create") // GET
